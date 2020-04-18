dc1d6ebc31ec442a52cd3efa909ba6cb2710a761
Danilo <danilo.test@gmail.com>
2020-04-18 03:43:37 +0200

    little updates

--- src/routes/formSuccess.js
+++ src/routes/formSuccess.js
@@ -4,7 +4,7 @@ export default function FormSuccess() {
 			<p class="text-2xl">Grazie per aver aggiunto l'attività!</p>
 			<p class="mt-5">
 				Abbiamo preso in carico la tua richiesta, normalmente la fase di approvazione richiede 24 ore.<br />
-				Non dimenticare di condividere con i tuoi amici quest'app per fare in modo che tutte le piccole attività e non, in provincia di Salerno, siano presenti!
+				Non dimenticare di condividere con i tuoi amici quest'app per fare in modo che tutte le piccole attività e non, nella città di Eboli e dintorni, siano presenti!
 			</p>
 			<span class="text-5xl" aria-label="thanks icons">🙏</span>
 		</div>
